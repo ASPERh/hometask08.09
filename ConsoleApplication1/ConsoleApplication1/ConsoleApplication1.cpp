4ff0b870776eb13e31d6e7bac7ec0d9a8507b274
ASPER <144143767+ASPERh@users.noreply.github.com>
2023-09-12 14:55:53 +0100

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,51 @@
+﻿//номер 1 
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	const int rows = 5;
+	const int cols = 5;
+	int rates[rows][cols];
+	for (int y = 0; y < rows; y++)
+	{
+		for (int x = 0; x < cols; x++)
+		{
+			rates[y][x] = 0;
+			if (x == 0) 
+				rates[y][x] = 1;
+			if (y == 0)
+				rates[y][x] = 1;
+			if (x == 4)
+				rates[y][x] = 1;
+			if (y == 4)
+				rates[y][x] = 1;
+			cout << rates[y][x];
+		}
+		cout << "\n";
+	}
+
+}
+
+//номер 2
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	const int rows = 10;
+	const int cols = 10;
+	int rates[rows][cols];
+	for (int y = 0; y < rows; y++)
+	{
+		for (int x = 0; x < cols; x++)
+		{
+			rates[y][x] = 0;
+			if (x == y or x == 9 - y)
+				rates[y][x] = 1;
+			cout << rates[y][x];
+		}
+		cout << "\n";
+	}
+
+}
